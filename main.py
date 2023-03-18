e5c0a25841d964b342f7e486cc5b8117cc205792
Harrison <47501747+Code-acus@users.noreply.github.com>
2023-03-18 13:43:26 -0600

    Update main.py

--- main.py
+++ main.py
@@ -31,10 +31,31 @@ print("Voila version: ", vo.__version__)
 # and the machine learning algorithm.
 #
 # Collection
-# In this stage the application's dataset retrieved from Kaggle is converted from ' \
-# a CSV to a Pandas DataFrame. Information about the DataFrame is then output for inspection.
+# In this stage the application's dataset retrieved from Kaggle is converted from a CSV file to a Pandas DataFrame.
+# Information about the DataFrame is then output for inspection.
 
-df = pd.read_csv('C:\Users\hrogers\PycharmProjects\C964_Capstone-Diabetes-ML-Predicator\diabetes_data.csv')
+df = pd.read_csv('C:\\Users\\hrogers\\PycharmProjects\\C964_Capstone-Diabetes-ML-Predicator\\diabetes_data.csv')
 df.info()
 
 # Preparation
+# In this stage, the data is prepared for use in the application.
+# As seen below, the dataset that has been collected has already been well prepared.
+# Their is no need for preparing data parsing, cleaning, or featurization, or data wrangling.
+# With the data prepared,it can effectively be used for the descriptive and non-descriptive portions
+# of the application.application
+
+df.head(5)
+
+# Renaming the columns to match the data
+df.rename({
+    'Pregnancies': 'Pregnancies',
+    'Glucose': 'Glucose',
+    'BloodPressure': 'BloodPressure',
+    'SkinThickness': 'SkinThickness',
+    'Insulin': 'Insulin',
+    'BMI': 'BMI',
+    'DiabetesPedigreeFunction': 'DiabetesPedigreeFunction',
+    'Age': 'Age',
+    'Outcome': 'Outcome'
+}, axis=1, inplace=True)
+
